014b07dba1b7d521589fea12ceb1a39c3ca6a0af
Nahummichael <nahum.michael@hgs.hiddengeniusproject.org>
2026-03-20 16:43:25 -0700

    mobile dev lesson 2

---
+++ Mobile Dev/Spring_Pod/Spring_Pod/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Spring_Pod
+//
+//  Created by Nahum Michael on 3/18/26.
+//
+
+import SwiftUI
+
+struct session1: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "star")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Nahum Michael")
+            
+            
+            Image("fbasketball")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 150)
+
+                
+                
+            
+            
+            Text("I am born in Oakland, and raised in Pinole, I proud of who i am, and what makes me. I have an interest in both video games and video game creation, and so i aspire to be able to create my own game at some point in the future.")
+            
+            
+            
+            
+            
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    session1()
+}
---
+++ Mobile Dev/Spring_Pod/Spring_Pod/Spring_PodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spring_PodApp.swift
+//  Spring_Pod
+//
+//  Created by Nahum Michael on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct Spring_PodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            session1()
+        }
+    }
+}
